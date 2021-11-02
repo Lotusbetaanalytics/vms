529f503875507203bd267b05e28487149b50993f
iCode <gbengaobafemi1@gmail.com>
2021-11-02 07:38:52 +0100

    updates

--- controllers/prebook.js
+++ controllers/prebook.js
@@ -25,28 +25,27 @@ exports.prebook = asyncHandler(async (req, res, next) => {
                   <tbody>
                       <tr>
                           <td align="center" valign="top" bgcolor="#640ad2"
-                              style="background:linear-gradient(0deg, rgba(100, 10, 210, 0.8), rgba(100, 10, 210, 0.8)),url(https://lbanstaffportal.herokuapp.com/static/media/tech.45a93050.jpg);background-size:cover; background-position:top;height:230">
+                              style="background:linear-gradient(0deg, rgba(100, 10, 210, 0.8), rgba(100, 10, 210, 0.8)),url(https://www.firs.gov.ng/wp-content/uploads/2020/11/firs-logo-3-300x54.png);background-size:cover; background-position:top;height:230">
                               <table class="col-600" width="600" height="200" border="0" align="center"
                                   cellpadding="0" cellspacing="0">
                                   <tbody>
                                       <tr>
                                           <td align="center" style="line-height: 0px;">
                                               <img style="display:block; line-height:0px; font-size:0px; border:0px;"
-                                                  src="https://lbanstaffportal.herokuapp.com/static/media/logo.49e95c77.png"
+                                                  src="https://www.firs.gov.ng/wp-content/uploads/2020/11/firs-logo-3-300x54.png"
                                                   width="70" height="70" alt="logo">
                                           </td>
                                       </tr>
                                       <tr>
                                           <td align="center"
                                               style="font-family: 'Raleway', sans-serif; font-size:37px; color:#ffffff;font-weight: bold;">
-                                              Lotus Beta Analytics
+                                             FIRS
                                           </td>
                                       </tr>
                                       <tr>
                                           <td align="center"
                                               style="font-family: 'Lato', sans-serif; font-size:15px; color:#ffffff;font-weight: 300;">
-                                              Our goal as an organization is to provide our customers with the best
-                                              value
+                                          
                                           </td>
                                       </tr>
                                   </tbody>
